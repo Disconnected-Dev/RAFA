4b038359d2147991f7a15db6ce135dd1551ed52e
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-02-19 16:49:53 +0000

    Fixing Issue

---
+++ pages/api/cognito.js
@@ -0,0 +1,11 @@
+import CognitoProvider from "next-auth/providers/cognito";
+
+export default NextAuth({
+    providers: [
+        CognitoProvider({
+            clientId: process.env.COGNITO_CLIENT_ID,
+            clientSecret: process.env.COGNITO_CLIENT_SECRET,
+            issuer: process.env.COGNITO_ISSUER,
+        })
+    ]
+})
