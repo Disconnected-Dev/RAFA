39c595d0671dcec0bda704660b1a4df62435c781
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-02-19 20:46:44 +0000

    Fixing Merge issue

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import CognitoProvider from "next-auth/providers/cognito"
+require('dotenv').config();
+
+export default NextAuth({
+    providers: [
+        CognitoProvider({
+          clientId: process.env.COGNITO_CLIENT_ID,
+          clientSecret: process.env.COGNITO_CLIENT_SECRET,
+          issuer: process.env.COGNITO_ISSUER,
+        })
+      ]
+})
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function HomePage() {
+  const { data: session, status } = useSession()
+
   const Map = dynamic(
-    () => import('../component/Map'), // replace '@components/map' with your component's location
-    { 
+    () => import("../component/Map"), // replace '@components/map' with your component's location
+    {
       loading: () => <p>A map is loading</p>,
-      ssr: false // This line is important. It's what prevents server-side render
+      ssr: false, // This line is important. It's what prevents server-side render
     }
-  )
-  return <Map />
+  );
+
+  if (status == "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (session) {
+    return (
+      <>
+        signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+        <Map />
+      </>
+    );
+  }
+  return (
+    <div>
+      <button onClick={() => signIn()}>Sign in</button>
+      <Map />
+    </div>
+  );
 }
-export default HomePage
+export default HomePage;
